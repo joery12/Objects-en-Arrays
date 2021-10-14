fe4119101934c534850a4a52a075cc00f91b0663
joery12 <90188965+joery12@users.noreply.github.com>
2021-10-14 19:39:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let person = { name: 'Joery', age: 40 };
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+let cijfers = { evaluations: '7, 10, 9' };
+console.log(cijfers.evaluations);
+
+let colors = ['groen', 'blauw', 'rood'];
+console.log(colors);
+console.log(colors.length);
+console.log(colors[0]);
+console.log(colors[colors.length - 1]);
+colors.push('geel');
+colors.push(5);
+colors.push({ greeting: "Hi ik ben een object" });
+console.log(colors);
+console.log(colors[5]);
